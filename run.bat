c9a23ab9bfb0e38154f5e2f79bce131730d2183f
xomayax <87386122+xomayax@users.noreply.github.com>
2023-04-22 07:29:49 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,8 +2,11 @@
 javac *.java
 ECHO normal file is called Normal.arxml
 ECHO empty file is called Empty.arxml
-ECHO invalid file is called new.c
-java Test 
+ECHO invalid file is called new.c 
+ECHO will test them in the same sequence
+java Test Normal.arxml
+java Test Empty.arxml
+java Test new.c
 pause
 
 
